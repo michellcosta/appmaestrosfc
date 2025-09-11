d2dbff7567690ee1648d8dc524f7f84498cba18e
michellcosta <michellcosta1269@gmail.com>
2025-09-11 19:52:49 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
-// src/main.tsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App' // <- troque de "./App.tsx" para "./App"
+import App from './App' // <- sem extensão .tsx
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
