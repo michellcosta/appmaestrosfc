3b9185ba2cde0797dc90b2a411b871006a3e0001
michellcosta <michellcosta1269@gmail.com>
2025-09-18 00:37:04 -0300

    revert: restaura páginas a partir dos backups .bak (App, Match, Finance, Ranking)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-﻿import {Routes, Route, Routes,  Route, Routes, Route, Routes,  Route,  Routes, Route } from "react-router-dom";
+﻿import {Routes, Route, Routes,  Route,  Routes, Route } from "react-router-dom";
 import Match from "src/pages/Match";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import {Routes, Route, Routes,  Route,  BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AppLayout } from "./components/layout/app-layout";
 import { Matches } from "./pages/Matches";
 import { Match } from "./pages/Match";
