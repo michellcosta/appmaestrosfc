5dfdadb7d449d4a971340a5c02f392e8ced85da8
michellcosta <michellcosta1269@gmail.com>
2025-09-19 11:25:10 -0300

    feat: reintroduzir AppRouter completo com basename

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import TestSimple from './TestSimple'
+import { BrowserRouter } from 'react-router-dom'
+import AppRouter from './AppRouter'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <TestSimple />
+    <BrowserRouter basename="/appmaestrosfc">
+      <AppRouter />
+    </BrowserRouter>
   </React.StrictMode>,
 )
--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { copyFileSync } from "fs";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/',
+  base: '/appmaestrosfc/',
   server: {
     host: "::",
     port: 8080,
